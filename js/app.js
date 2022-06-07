495787a53308e2b2f4bf9431ec245abb66a5bbd7
Ruma Das <rumaadl16@gmail.com>
2022-06-07 21:35:51 +0930

    adding animation

--- js/app.js
+++ js/app.js
@@ -1,5 +1,27 @@
-const item = document.querySelector('#shape')
+// const item = document.querySelector('#shape')
 
-console.log(item.getTotalLength())
+// console.log(item.getTotalLength())
 
 //console.log(window.screen)
+const circle = document.querySelector('.circle')
+const lines = document.querySelector('.lines')
+const list = document.querySelector('.list')
+// const line1 = document.querySelector('.line-1')
+// const line2 = document.querySelector('.line-2')
+// const line3 = document.querySelector('.line-3')
+
+const lineAll = document.querySelectorAll('.line')
+
+console.log(circle)
+console.log('new day')
+lines.addEventListener('click', () => {
+  circle.classList.toggle('active')
+  list.classList.toggle('active')
+
+  //   line1.classList.toggle('active')
+  //   line2.classList.toggle('active')
+  //   line3.classList.toggle('active')
+  for (const line of lineAll) {
+    line.classList.toggle('active')
+  }
+})
